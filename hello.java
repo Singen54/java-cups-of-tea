c7b578ee26f6d8935a332eb033e11400d17006c8
Soso54 <davedghg193@gmail.com>
2024-12-20 19:57:16 +0200

    Create hello.java
    حيث يتم تعمير برنامج بلغت جافا

---
+++ hello.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ public class k {
+   static Scanner cin = new Scanner(System.in);
+  public static void main(String[] args) throws java.io.IOException{
+
+   int g;
+   System.out.println("enter your name");
+   g = cin.nextInt();
+   System.out.println(g);
+
+
+
+
+
+
+
+
+
+ }
+}
